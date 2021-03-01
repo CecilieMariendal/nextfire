bbe23de4c900ad240910e54accf57e2d70874090
Cecilie Mariendal <ctmariendal@gmail.com>
2021-03-01 21:16:36 +0100

    Add edit page

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 
 export default function PostContent({post}) {
-    const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
+    const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt?.toDate();
+    
     
     return (
         <div className="card">
@@ -12,7 +13,7 @@ export default function PostContent({post}) {
                 <Link href={`/${post.username}/`}>
                     <a className="text-info">@{post.username}</a>
                 </Link>{' '}
-                on {createdAt.toISOString()}
+                on {createdAt?.toISOString()}
             </span>
             <ReactMarkdown>{post?.content}</ReactMarkdown>
         </div>
