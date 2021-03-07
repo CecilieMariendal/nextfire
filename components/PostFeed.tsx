2aa1d908627ee86c9521fcba959057bb1a08bdb2
Cecilie Mariendal <ctmariendal@gmail.com>
2021-03-07 21:56:29 +0100

    Home page improvements

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,47 +1,43 @@
 import Link from 'next/link';
+import { PostType } from '../@types/post.type';
+import styles from '../styles/PostFeed.module.css'
 
 
 export default function PostFeed({ posts, admin = false }) {
-    return (posts) ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;    
+    return (posts) ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
 }
 
 
-function PostItem({post, admin = false}) {
+function PostItem({ post, admin = false }: { post: PostType, admin: boolean }) {
     // Word count and read time
     const wordCount = post?.content.trim().split(/\s+/g).length;
     const minutesToRead = (wordCount / 100 + 1).toFixed(0);
-    
+
+
     return (
-        <div className="card">¨
-            <Link href={`/${post.username}`}>
-                <a>
-                    <strong>By @{post.username}</strong>
-                </a>
-            </Link>
+        <Link href={`/${post.username}/${post.slug}`}>
+            <a className={styles.card}>
+                <p>By @{post.username}</p>
+                <h2>{post.title}</h2>
 
-            <Link href={`/${post.username}/${post.slug}`}>
-                <h2>
-                    <a>{post.title}</a>
-                </h2>
-            </Link>
+                <footer>
+                    <span>{wordCount} words. {minutesToRead} min read</span>
+                    <span className="hearts">💗 {post.heartCount || 0} Hearts</span>
+                </footer>
 
-            <footer>
-                <span>{wordCount} words. {minutesToRead} min read</span>
-                <span className="hearts">💗 {post.heartCount || 0} Hearts</span>
-            </footer>
+                {/* If admin view, show extra controls for user */}
+                {admin && (
+                    <>
+                        <Link href={`/admin/${post.slug}`}>
+                            <h3>
+                                <button className="btn-blue">Edit</button>
+                            </h3>
+                        </Link>
 
-            {/* If admin view, show extra controls for user */}
-            {admin && (
-                <>
-                <Link href={`/admin/${post.slug}`}>
-                    <h3>
-                        <button className="btn-blue">Edit</button>
-                    </h3>
-                </Link>
-
-                {post.published ? <p className="text-success">Live</p> : <p className="text-danger">Unpublished</p>}
-                </>
-            )}
-        </div>
+                        {post.published ? <p className="text-success">Live</p> : <p className="text-danger">Unpublished</p>}
+                    </>
+                )}
+            </a>
+        </Link>
     );
 }
